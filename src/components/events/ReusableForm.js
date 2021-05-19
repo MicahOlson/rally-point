d1acd585b8d902dce64dacf7a9816ea3165eaf46
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-18 17:28:30 -0700

    Add ResuableForm component

---
+++ src/components/events/ReusableForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='notification'
+          placeholder='Enter notification here'
+        />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
