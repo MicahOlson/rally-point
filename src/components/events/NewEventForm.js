fa418c9df08773657388db4f2fd1a43f16d16b41
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-18 17:28:56 -0700

    Add NewEventForm component

---
+++ src/components/events/NewEventForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
+
+function NewEventForm(props) {
+  function handleNewEventFormSubmission(event) {
+    event.preventDefault();
+    props.onNewEventCreation({
+      notification: event.target.notification.value,
+    });
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewEventFormSubmission}
+        buttonText="Add Event"
+      />
+    </React.Fragment>
+  );
+}
+
+NewEventForm.propTypes = {
+  onNewEventCreation: PropTypes.func
+};
+
+export default NewEventForm;
