6135175b3714769cfdea875c97fbc34ad2f0201a
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-18 17:16:39 -0700

    Add style wrapper and dynamic links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavBarWrapper = styled.section`
+  background-color: #000000;
+  width: 100%;
+  font-size: 24px;
+  color: #ffffff;
+  text-align: center;
+  padding-top: 24px;
+  padding-bottom: 24px;
+`
 
 const NavBar = props => {
   const handleLogoutClick = () => {
@@ -13,13 +24,19 @@ const NavBar = props => {
     })
   }
 
+  // props.handleGettingOrganizationName(props.user.organization_id)
+
   if (props.loggedInStatus === "NOT_LOGGED_IN") {
     return (
-      <div><Link to="/">Home</Link> | <a href="/#contact">Contact Us</a> | <Link to="/login">Log In</Link></div>
+      // <NavBarWrapper>
+        <div><Link to="/">Home</Link> | <a href="/#contact">Contact Us</a> | <Link to="/login">Log In</Link></div>
+      // </NavBarWrapper>
     )
   } else {
     return (
-      <div><Link to="/">Home</Link> | <a href="/#contact">Contact Us</a> | Organization | Hi, {props.user.first_name} | <a href="" onClick={() => handleLogoutClick()}>Log Out</a></div>
+      // <NavBarWrapper>
+        <div><Link to="/">Home</Link> | <a href="/#contact">Contact Us</a> | <Link to="/app">Organization</Link> | Hi, {props.user.first_name} | <a href="" onClick={() => handleLogoutClick()}>Log Out</a></div>
+      // </NavBarWrapper>
     )
   }
 }
